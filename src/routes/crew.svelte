f024574f217bdbf27a706e7bbf0df216d4f736f7
James Hannan <jim9112@gmail.com>
2022-04-09 11:04:00 -0400

    start crew page

--- src/routes/crew.svelte
+++ src/routes/crew.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
 	import Main from '../components/Main.svelte';
+	import { crew } from '../lib/data';
+	let crewIndex = 0;
+	console.log(crew);
 </script>
 
 <Main pageType="crew">
-	<div>
-		<h1 class="text-white">Crew</h1>
+	<div class="flex flex-col">
+		<h1 class="text-white">02 Meet The Crew</h1>
+
+		<div class="grid grid-cols-2 w-screen">
+			<div class="flex flex-col items-center">
+				<div class="flex flex-col max-w-md">
+					<h3>{crew[crewIndex].role}</h3>
+					<h2>{crew[crewIndex].name}</h2>
+					<p>{crew[crewIndex].bio}</p>
+				</div>
+				<ul class="flex gap-4">
+					<li>0</li>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+				</ul>
+			</div>
+			<div class="flex flex-col items-center">
+				<img src={crew[crewIndex].images.webp} alt={crew[crewIndex].name} />
+			</div>
+		</div>
 	</div>
 </Main>
