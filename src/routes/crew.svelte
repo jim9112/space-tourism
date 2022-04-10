5db2752bf7f32f94941df5e5b6247212244f2ed7
James Hannan <jim9112@gmail.com>
2022-04-10 09:09:25 -0400

    add technology page

--- src/routes/crew.svelte
+++ src/routes/crew.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
+	import BodyText from '../components/BodyText.svelte';
 	import Main from '../components/Main.svelte';
 	import { crew } from '../lib/data';
 	let crewIndex = 0;
-	console.log(crew);
+
+	const handleClick = (e) => {
+		if (e.target.nodeName === 'LI') {
+			crewIndex = parseInt(e.target.dataset.index);
+		}
+	};
 </script>
 
 <Main pageType="crew">
 	<div class="flex flex-col">
-		<h1 class="text-white">02 Meet The Crew</h1>
+		<h1 class="text-white font-barlow text-[28px] uppercase">
+			<span class="text-grey">02</span> Meet The Crew
+		</h1>
 
 		<div class="grid grid-cols-2 w-screen">
 			<div class="flex flex-col items-center">
-				<div class="flex flex-col max-w-md">
-					<h3>{crew[crewIndex].role}</h3>
-					<h2>{crew[crewIndex].name}</h2>
-					<p>{crew[crewIndex].bio}</p>
+				<div class="flex flex-col max-w-lg">
+					<h3 class="font-bellefair text-[32px] text-grey uppercase">{crew[crewIndex].role}</h3>
+					<h2 class="font-bellefair text-[56px] uppercase">{crew[crewIndex].name}</h2>
+					<BodyText>{crew[crewIndex].bio}</BodyText>
 				</div>
-				<ul class="flex gap-4">
-					<li>0</li>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
+				<ul class="flex gap-4" on:click={handleClick}>
+					<li class="h-4 w-4 rounded-full bg-grey cursor-pointer" data-index="0" />
+					<li class="h-4 w-4 rounded-full bg-grey cursor-pointer" data-index="1" />
+					<li class="h-4 w-4 rounded-full bg-grey cursor-pointer" data-index="2" />
+					<li class="h-4 w-4 rounded-full bg-grey cursor-pointer" data-index="3" />
 				</ul>
 			</div>
 			<div class="flex flex-col items-center">
--- src/routes/technology.svelte
+++ src/routes/technology.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
+	import BodyText from '../components/BodyText.svelte';
 	import Main from '../components/Main.svelte';
+	import { technology } from '../lib/data';
+	let techIndex = 0;
 </script>
 
 <Main pageType="technology">
-	<div>
-		<h1 class="text-white">Technology</h1>
+	<div class="flex flex-col">
+		<h1 class="text-white font-barlow text-[28px] uppercase">
+			<span class="text-grey">03</span> Space Launch 101
+		</h1>
+
+		<div class="flex">
+			<div class="flex">
+				<ul>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+				</ul>
+				<div class="max-w-lg">
+					<p class="font-barlow text-text-primary text-base">The Terminology</p>
+					<h2 class="font-bellefair text-[56px] uppercase">{technology[techIndex].name}</h2>
+					<BodyText>{technology[techIndex].description}</BodyText>
+				</div>
+			</div>
+			<div>
+				<img src={technology[techIndex].images.portrait} alt={technology[techIndex].name} />
+			</div>
+		</div>
 	</div>
 </Main>
