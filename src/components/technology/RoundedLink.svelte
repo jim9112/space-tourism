36759928fec77c9d2e1516842218cd71dca246fd
James Hannan <jim9112@gmail.com>
2022-04-13 18:33:33 -0400

    some styling on technology page

---
+++ src/components/technology/RoundedLink.svelte
@@ -0,0 +1,3 @@
+<li class="w-20 h-20 border-[1px] border-white rounded-full flex items-center justify-center">
+	<slot />
+</li>
--- src/routes/technology.svelte
+++ src/routes/technology.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
 	import BodyText from '../components/BodyText.svelte';
 	import Main from '../components/Main.svelte';
+	import RoundedLink from '../components/technology/RoundedLink.svelte';
 	import { technology } from '../lib/data';
 	let techIndex = 0;
 </script>
 
 <Main pageType="technology">
-	<div class="flex flex-col">
+	<div class="flex flex-col pt-52">
 		<h1 class="text-white font-barlow text-[28px] uppercase">
 			<span class="text-grey">03</span> Space Launch 101
 		</h1>
 
-		<div class="flex">
-			<div class="flex">
-				<ul>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
+		<div class="flex w-screen justify-end gap-20">
+			<div class="flex gap-20">
+				<ul class="flex flex-col gap-8">
+					<RoundedLink>1</RoundedLink>
+					<RoundedLink>2</RoundedLink>
+					<RoundedLink>3</RoundedLink>
 				</ul>
 				<div class="max-w-lg">
 					<p class="font-barlow text-text-primary text-base">The Terminology</p>
